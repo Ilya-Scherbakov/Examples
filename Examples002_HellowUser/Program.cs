44915187efb4e3ec0dbf4bffe1de5005296e13c2
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-05 20:11:57 +0400

    Add Examples 005 Нахождение максимального числа

--- Examples002_HellowUser/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Делаем приветствие пользователя с запросом его имени
-Console.WriteLine("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
