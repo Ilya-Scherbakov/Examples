4f63bbe91be7990433c9b91228bd116a0a757986
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-05 19:08:24 +0400

    сумма случайных чисел

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Create programm суммы two numbers
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int sum = numberA + numberB;
 Console.WriteLine(sum);
 
