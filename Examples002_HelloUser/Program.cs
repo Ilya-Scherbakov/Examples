6fcdfc580a194e90ccdb2ab23feb9e862fdfe5f7
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-05 20:15:19 +0400

    Add Examples 002 приветствие с запросом имени

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿// Делаем приветствие пользователя с запросом его имени
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
