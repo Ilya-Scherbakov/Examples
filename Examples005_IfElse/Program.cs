3e525dc1e45f9229d573b4fc3086873e8f6cf8bd
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-05 19:32:14 +0400

    Add Example 005 приветствие персональное

---
+++ Examples005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Приветствие любимчика по имени
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура! Это же ");
+    Console.WriteLine(username);
+}
+else
+{
+    Console.Write("Привет: ");
+    Console.WriteLine(username);
+}
