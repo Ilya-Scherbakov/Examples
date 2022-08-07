a7cd8f57ffa2542e9c712de06ca3d199307cb204
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 20:58:58 +0400

    Add examples 011 нахождение эл-та массива равного определнному значению

--- Examples011_MethodArray/Program.cs
+++ Examples011_MethodArray/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Имеется массив array из n элементов. Требуется найти эл-т массива, равный find
 
+int[] array = { 12, 42, 32, 84, 51, 66, 57, 81 };
+int n = array.Length;
+int find = 81;
+int index = 0;
 
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
